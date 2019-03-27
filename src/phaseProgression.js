1c2353575b77a3396f07af8ecdc1b494c4264ad4
rajivmehtapy <rajiv.mehta@bacancytechnology.com>
2019-03-27 15:26:43 +0530

    Add Routes

---
+++ src/phaseProgression.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class PhaseProgression extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        );
+    }
+}
+
+export default PhaseProgression;
